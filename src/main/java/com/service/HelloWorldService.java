5816be1869a7d5c7d139ec6fdbf942ed83378b98
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-05-10 13:40:34 +0300

    QueryParam kullanıldı.

--- src/main/java/com/service/HelloWorldService.java
+++ src/main/java/com/service/HelloWorldService.java
@@ -3,17 +3,44 @@ package com.service;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/message")
 public class HelloWorldService {
 
     @GET
     @Path("/{param}")
-    public Response getMsg(@PathParam("param") String msg) {
+    public Response getMsg(@PathParam("param") String msg) {//@Path param örneği
 
         String output = "Message : " + msg;
 
         return Response.status(200).entity(output).build();
     }
+    //url ye alttaki örnek gibi yazılacaktır
+    //http://localhost:8080/msg/message/naber
+
+    @GET
+    @Path("/person")
+    public Response getName(@QueryParam("name") String name) {//@Query param örneği
+
+        String output = "Name : " + name;
+
+        return Response.status(200).entity(output).build();
+    }
+    //url ye alttaki örnek gibi yazılacaktır
+    //http://localhost:8080/msg/message/person?name=Muhammed
+
+    @GET
+    @Path("/person1")
+    public Response getNameSurname(@QueryParam("name") String name, @QueryParam("surname") String surname) {
+        String output = "Name : " + name;
+        output += "\n Surname : " + surname;
+
+        return Response.status(200).entity(output).build();
+    }
+    //url ye alttaki örnek gibi yazılacaktır
+    //http://localhost:8080/msg/message/person1?name=Muhammed&surname=Demir
+    
+
 }
