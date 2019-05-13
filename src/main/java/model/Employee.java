6310be0ce54438977e03707d9116f3c3ea417ad4
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-05-14 00:24:02 +0300

    Jackson örneği eklendi.

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Employee {
+    int id;
+    String name;
+    String surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
